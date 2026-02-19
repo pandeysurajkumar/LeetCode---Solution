a0a72cada8ee28ce37e95505326ad414bcf828d0
pandeysurajkumar <145700588+pandeysurajkumar@users.noreply.github.com>
2026-02-19 15:18:10 +0530

    Time: 0 ms (100%), Space: 7.7 MB (64.22%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,29 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1 ) return 1;
-        return fib(n-1)+fib(n-2);
+        if (n <= 1) return n;
+
+        int prev2 = 0; // F(0)
+        int prev1 = 1; // F(1)
+
+        for (int i = 2; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
     }
 };
+
+
+//byrecursion
+
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n==0) return 0;
+//         if(n==1 ) return 1;
+//         return fib(n-1)+fib(n-2);
+//     }
+// };
