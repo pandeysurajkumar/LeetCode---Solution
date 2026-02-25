8e3647b4244691a49f435f13c8af29002b1cbf38
pandeysurajkumar <145700588+pandeysurajkumar@users.noreply.github.com>
2026-02-25 13:02:02 +0530

    Time: 0 ms (100%), Space: 7.8 MB (58.85%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,20 +1,38 @@
+// class Solution {
+// public:
+//     int bitwiseComplement(int n) {
+//         if(n<=0) return 1;
+//        int result=0;
+//        int i =0;
+//        while(n!=0){
+//         int bit = n & 1;
+//         if(bit != 1){
+//             result = (1 * pow(2,i)) + result;
+//             i++;
+//             n = n>>1;
+//         }else{
+//             i++;
+//             n=n>>1;
+//         }
+//        }
+//        return result; 
+//     }
+// };
+
+
+
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        if(n<=0) return 1;
-       int result=0;
-       int i =0;
-       while(n!=0){
-        int bit = n & 1;
-        if(bit != 1){
-            result = (1 * pow(2,i)) + result;
-            i++;
-            n = n>>1;
-        }else{
-            i++;
-            n=n>>1;
+        if(n<=0)return 1;
+        int mass=0;
+        int m = n;
+        while(m!=0){
+            mass = (mass<<1)|1;
+            m = m>>1;
         }
-       }
-       return result; 
+        int res = (~n)& mass;
+        return res;
+
     }
 };
