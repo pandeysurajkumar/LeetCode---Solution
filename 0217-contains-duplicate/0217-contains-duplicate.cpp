597886a889d74cae4e57c7dc7467bcb6742c8322
pandeysurajkumar <145700588+pandeysurajkumar@users.noreply.github.com>
2026-03-09 14:15:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int> s(nums.begin(),nums.end());
-        if(s.size()!= nums.size()) return true;
+        unordered_set<int> s;
+        for(auto it:nums){
+            if(s.count(it)) return true;
+            s.insert(it);
+        }
         return false;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         set<int> s(nums.begin(),nums.end());
+//         if(s.size()!= nums.size()) return true;
+//         return false;
+//     }
+// };
