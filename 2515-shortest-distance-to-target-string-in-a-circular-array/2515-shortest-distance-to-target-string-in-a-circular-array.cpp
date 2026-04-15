429f9b94e4cd0757b197582d84f642cbeba86e31
pandeysurajkumar <145700588+pandeysurajkumar@users.noreply.github.com>
2026-04-15 13:29:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
     int closestTarget(vector<string>& words, string target, int startIndex) {
-        int front = INT_MAX;
-        int back = INT_MAX;
         int n = words.size();
-        for(int i= 0;i<n;i++){
-            if(words[(startIndex+i)%n] ==target){
-                front = i;
-                break;
+        int ans = INT_MAX;
+        for(int i=0;i<n;i++){
+            if(words[i]==target){
+                int diff = abs(startIndex - i);
+                ans = min(ans,min(diff,(n-diff)));
             }
         }
-        for(int i = 0;i<n;i++){
-            if(words[(startIndex-i+n)%n] ==target){
-                back = i;
-                break;
-            }
-        }
-        int ans = min(front,back);
         return ans == INT_MAX ? -1 : ans;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     int closestTarget(vector<string>& words, string target, int startIndex) {
+//         int front = INT_MAX;
+//         int back = INT_MAX;
+//         int n = words.size();
+//         for(int i= 0;i<n;i++){
+//             if(words[(startIndex+i)%n] ==target){
+//                 front = i;
+//                 break;
+//             }
+//         }
+//         for(int i = 0;i<n;i++){
+//             if(words[(startIndex-i+n)%n] ==target){
+//                 back = i;
+//                 break;
+//             }
+//         }
+//         int ans = min(front,back);
+//         return ans == INT_MAX ? -1 : ans;
+//     }
+// };
