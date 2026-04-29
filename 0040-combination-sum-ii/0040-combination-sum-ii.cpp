a558dcd4f909a345bc6fdcfe02623786ee3e5ee6
pandeysurajkumar <145700588+pandeysurajkumar@users.noreply.github.com>
2026-04-29 14:10:08 +0530

    Time: 0 ms (100%), Space: 14.1 MB (59.45%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void find(int index, int target, vector<int>& arr, vector<vector<int>>& ans, vector<int>& ds) {
+        
+        if(target == 0) {
+            ans.push_back(ds);
+            return;
+        }
+
+        for(int i = index; i < arr.size(); i++) {
+
+            // skip duplicates
+            if(i > index && arr[i] == arr[i-1]) continue;
+
+            if(arr[i] > target) break;
+
+            ds.push_back(arr[i]);
+            find(i + 1, target - arr[i], arr, ans, ds);
+            ds.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+
+        sort(candidates.begin(), candidates.end()); // important
+
+        find(0, target, candidates, ans, ds);
+        return ans;
+    }
+};
