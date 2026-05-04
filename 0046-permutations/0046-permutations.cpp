c606d39c1470ff3adb22e0259c77abf3b2d00270
pandeysurajkumar <145700588+pandeysurajkumar@users.noreply.github.com>
2026-05-04 15:13:09 +0530

    Time: 0 ms (100%), Space: 10.6 MB (68.2%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -8,6 +8,7 @@ public:
         }
         for(int i=0;i<nums.size();i++){
             if(used[i]) continue;
+            if(i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
 
             used[i]=true;
             dp.push_back(nums[i]);
@@ -19,6 +20,7 @@ public:
 
     vector<vector<int>> permute(vector<int>& nums) {
         int n = nums.size();
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         vector<int>dp;
         vector<bool> used(n,false);
