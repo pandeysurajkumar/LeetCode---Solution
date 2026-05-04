4733b43dcb995128efce8ef1e5d351f4f4765478
pandeysurajkumar <145700588+pandeysurajkumar@users.noreply.github.com>
2026-05-04 16:01:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        // next_permutation(nums.begin(),nums.end());  // STL Library for next_permutation i.e.Better solution
+
+        int index = -1;
+        int n = nums.size();
+
+        //find break point
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1){
+            reverse(nums.begin(),nums.end());
+           return;
+        }
+
+        // find min and swap
+        for(int i = n-1;i>index;i--){
+            if(nums[i]>nums[index]){
+                swap(nums[i],nums[index]);
+                break;
+            }
+        }
+
+        // reverse rest of it
+        reverse(nums.begin()+index+1,nums.end());
+
         
     }
 };
