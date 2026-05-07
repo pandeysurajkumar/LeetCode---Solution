2c1eb00ff1930f0480f76fcfc56dc26b8e0cd09b
pandeysurajkumar <145700588+pandeysurajkumar@users.noreply.github.com>
2026-05-07 18:18:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,43 +1,11 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,vector<string> board,int n)
-    {
-        int rowStore = row;
-        int colStore = col;
-
-        // for upper
-        while(row >= 0 && col >= 0)
-        {
-            if(board[row][col]=='Q') return false;
-            row--;
-            col--;
-        }
-
-        // for back
-        row = rowStore;
-        col = colStore;
-        while(col >= 0)
-        {
-            if(board[row][col]=='Q') return false;
-            col--;
-        }
-
-        // for down
-        row = rowStore;
-        col = colStore;
-        while(row<n && col >= 0)
-        {
-            if(board[row][col]=='Q') return false;
-            row++;
-            col--;
-        }
-
-        return true;
-    }
-
-
-
-    void place(int col,vector<string> &board,vector<vector<string>> &ans,int n)
+    void place(int col,vector<string> &board,
+            vector<vector<string>> &ans,
+            vector<int> &upper,
+            vector<int> &left,
+            vector<int> &down,
+            int n)
     {
         if(col == n)
         {
@@ -47,15 +15,20 @@ public:
 
         for(int row=0;row<n;row++)
         {
-            if(isSafe(row,col,board,n))
+            if(upper[n-1 + (col-row)] == 0 && left[row] == 0 && down[row+col] == 0)
             {
                 board[row][col] = 'Q';
-                place(col+1,board,ans,n);
+                upper[n-1+(col-row)] = 1;
+                left[row] = 1;
+                down[row+col] = 1;
+                place(col+1,board,ans,upper,left,down,n);
                 board[row][col] = '.';
+                upper[n-1 + (col-row)] = 0;
+                left[row] = 0;
+                down[row+col] = 0;
             }
         }
     }
-
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string> board(n);
@@ -64,7 +37,92 @@ public:
         {
             board[i] = s;
         }
-        place(0,board,ans,n);
+        vector<int>upper(2*n-1,0);
+        vector<int>left(n,0);
+        vector<int>down(2*n-1,0);
+        place(0,board,ans,upper,left,down,n);
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     bool isSafe(int row,int col,vector<string> board,int n)
+//     {
+//         int rowStore = row;
+//         int colStore = col;
+
+//         // for upper
+//         while(row >= 0 && col >= 0)
+//         {
+//             if(board[row][col]=='Q') return false;
+//             row--;
+//             col--;
+//         }
+
+//         // for back
+//         row = rowStore;
+//         col = colStore;
+//         while(col >= 0)
+//         {
+//             if(board[row][col]=='Q') return false;
+//             col--;
+//         }
+
+//         // for down
+//         row = rowStore;
+//         col = colStore;
+//         while(row<n && col >= 0)
+//         {
+//             if(board[row][col]=='Q') return false;
+//             row++;
+//             col--;
+//         }
+
+//         return true;
+//     }
+
+
+
+//     void place(int col,vector<string> &board,vector<vector<string>> &ans,int n)
+//     {
+//         if(col == n)
+//         {
+//             ans.push_back(board);
+//             return;
+//         }
+
+//         for(int row=0;row<n;row++)
+//         {
+//             if(isSafe(row,col,board,n))
+//             {
+//                 board[row][col] = 'Q';
+//                 place(col+1,board,ans,n);
+//                 board[row][col] = '.';
+//             }
+//         }
+//     }
+
+//     vector<vector<string>> solveNQueens(int n) {
+//         vector<vector<string>> ans;
+//         vector<string> board(n);
+//         string s(n,'.');
+//         for(int i=0;i<n;i++)
+//         {
+//             board[i] = s;
+//         }
+//         place(0,board,ans,n);
+//         return ans;
+//     }
+// };
