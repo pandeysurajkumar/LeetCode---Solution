855f22c88399b8191803fbfdf799cf404ad8088a
pandeysurajkumar <145700588+pandeysurajkumar@users.noreply.github.com>
2026-05-23 13:54:00 +0530

    Time: 24 ms (86.33%), Space: 70.9 MB (96.41%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,49 +1,11 @@
-// class Solution {
-// public:
-//     bool containsDuplicate(vector<int>& nums) {
-//         unordered_map<int,int>freq;
-//         for(auto it:nums){
-//             freq[it]++;
-//         }
-//         for(auto it:freq){
-//             if(it.second != 1){
-//                 return true;
-//             }
-//         }
-//         return false;
-
-//     }
-// };
-
-
-
-
-
-
-
-
-
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> s;
-        for(auto it:nums){
-            if(s.count(it)) return true;
-            s.insert(it);
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==nums[i+1]) return true;
         }
         return false;
     }
 };
-
-
-
-
-
-// class Solution {
-// public:
-//     bool containsDuplicate(vector<int>& nums) {
-//         set<int> s(nums.begin(),nums.end());
-//         if(s.size()!= nums.size()) return true;
-//         return false;
-//     }
-// };
