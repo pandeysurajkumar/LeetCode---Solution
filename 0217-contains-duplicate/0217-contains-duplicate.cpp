aee1a307b16c4fe6e87e15bafac7323a04b733e4
pandeysurajkumar <145700588+pandeysurajkumar@users.noreply.github.com>
2026-05-23 13:55:57 +0530

    Time: 55 ms (31.77%), Space: 90.9 MB (44.58%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,29 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         for(int i=0;i<nums.size()-1;i++)
+//         {
+//             if(nums[i]==nums[i+1]) return true;
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++)
+
+        unordered_set<int> st;
+
+        for(int num : nums)
         {
-            if(nums[i]==nums[i+1]) return true;
+            if(st.find(num) != st.end())
+                return true;
+
+            st.insert(num);
         }
+
         return false;
     }
 };
