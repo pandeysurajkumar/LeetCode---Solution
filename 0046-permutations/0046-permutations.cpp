065e39052e42c96d8a6ef63b2212dbe0da22a4e3
pandeysurajkumar <145700588+pandeysurajkumar@users.noreply.github.com>
2026-07-16 10:46:09 +0530

    Time: 0 ms (100%), Space: 10.5 MB (88.79%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,84 +1,31 @@
 class Solution {
-
-        void permu(int index,vector<int>& nums,
-               vector<vector<int>>& ans){
-
-                if(index == nums.size())
-                {
-                    ans.push_back(nums);
-                    return;
-                }
-                for(int i=index;i<nums.size();i++)
-                {
-                    swap(nums[index],nums[i]);
-                    permu(index+1,nums,ans);
-                    // Backtracking
-                    swap(nums[index],nums[i]);
-                }
-
-               }
-
-
+private:
+    void solve(vector<int>& nums,vector<bool>& visited,vector<int>& temp,vector<vector<int>>& ans)
+    {
+        if(temp.size()==nums.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(visited[i])
+                continue;
+            
+            visited[i] = true;
+            temp.push_back(nums[i]);
+            solve(nums,visited,temp,ans);
+            temp.pop_back();
+            visited[i] = false;
+        }
+    }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
         vector<vector<int>> ans;
-        permu(0,nums,ans);
+        vector<int> temp;
+        vector<bool>visited(n,false);
+        solve(nums,visited,temp,ans);
         return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-
-//     void permu(vector<int>& nums,
-//                vector<bool>& used,
-//                vector<vector<int>>& ans,
-//                vector<int>& dp){
-
-//         // Base condition
-//         if(dp.size() == nums.size()){
-//             ans.push_back(dp);
-//             return;
-//         }
-
-//         for(int i=0; i<nums.size(); i++){
-
-//             // Skip used element
-//             if(used[i]) continue;
-
-//             used[i] = true;
-//             dp.push_back(nums[i]);
-
-//             permu(nums, used, ans, dp);
-
-//             // Backtrack
-//             dp.pop_back();
-//             used[i] = false;
-//         }
-//     }
-
-//     vector<vector<int>> permute(vector<int>& nums) {
-
-//         vector<vector<int>> ans;
-//         vector<int> dp;
-//         vector<bool> used(nums.size(), false);
-
-//         permu(nums, used, ans, dp);
-
-//         return ans;
-//     }
-// };
